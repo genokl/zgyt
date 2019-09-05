cbdd7b9a8cfca7fbf74498d9336415e4b93fbcba
genokl <1047152401@qq.com>
2019-09-05 20:02:47 +0800

    1

---
+++ newsreplite/src/main/java/cn/zgyt/NewsrepliteApplication.java
@@ -0,0 +1,13 @@
+package cn.zgyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsrepliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsrepliteApplication.class, args);
+	}
+
+}
